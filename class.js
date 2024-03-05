aad4ecd2d0e38198ec5a237b295d156b2bcd6a24
ShridattaB <shridattabhimanavaru@gmail.com>
2024-03-06 00:43:32 +0530

    progress

---
+++ class.js
@@ -0,0 +1,16 @@
+// Introduction to JavaScript Classes:
+// Theory: Explain the concept of classes in JavaScript and their role in object-oriented programming.
+// Practical: Write a simple JavaScript class definition with properties and methods and instantiate an object from it.
+//classes are nothing but blueprint of object
+//object are nothing but instance of class
+
+
+class person{
+    constructor(age,name){
+    this.age=age
+    this.name=name
+}
+
+}
+
+const john=new person('shre',38)
